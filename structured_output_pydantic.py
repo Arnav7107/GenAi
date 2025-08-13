50364f3845738af1e74ff0f03b8b2b7903cc11f3
Arnav Verma <shyamverma@Mac.lan>
2025-08-13 22:39:03 +0530

    used pydantic library

--- structured_output_pydantic.py
+++ structured_output_pydantic.py
@@ -1,17 +1,17 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from typing import TypedDict, Annotated, Optional
-from pydantic import BaseModel
+from typing import TypedDict, Annotated, Optional, Literal
+from pydantic import BaseModel, Field
 
 load_dotenv()
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 class Review(BaseModel):
-    key_themes : Annotated[list[str], "Write down all the key features discussed in the description in a list"]
-    summary : Annotated[str, "Give a breif summary of the given description"]
-    sentiment : Annotated[str, "Give the sentiment of the description either positive, negative or neutral"]
-    pros : Annotated[Optional[list[str]], "Write down the pros from the description if any in a list"]
-    cons : Annotated[Optional[list[str]], "Write down the cons from the description if any in a list"]
+    key_themes : list[str] =  Field(description = "Write down all the key features discussed in the description in a list")
+    summary : str = Field(description = "Give a breif summary of the given description")
+    sentiment : Literal["positive", "negative"] = Field(description =  "Give the sentiment of the description either positive, negative or neutral")
+    pros : Optional[list[str]] = Field(description = "Write down the pros from the description if any in a list")
+    cons : Optional[list[str]] = Field(description = "Write down the cons from the description if any in a list")
 
 structured_model = model.with_structured_output(Review)
 
